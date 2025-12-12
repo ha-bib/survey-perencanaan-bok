04d543488c3369d10b9c812b59f5ddab2fb3f2e0
Habz Dev <habiburrohman900@gmail.com>
2025-12-12 10:56:52 +0700

    favicon

--- resources/views/usulan/form.blade.php
+++ resources/views/usulan/form.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form Survey Perencanaan BOK 2027</title>
+    <link rel="icon" href="{{ asset('favicon.png') }}" type="image/png">
     <script src="https://cdn.tailwindcss.com"></script>
     <style>
         :root {
--- resources/views/usulan/indikator.blade.php
+++ resources/views/usulan/indikator.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Daftar Indikator Kesprimkom</title>
+    <link rel="icon" href="{{ asset('favicon.png') }}" type="image/png">
     <script src="https://cdn.tailwindcss.com"></script>
     <style>
         :root {
